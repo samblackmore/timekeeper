6d733c3356cea8fadd9864571d03aab942205af4
Sam Blackmore <samblackmore07@gmail.com>
2018-02-11 19:19:09 -0500

    adding firebase

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const http = require('http');
+var admin = require('firebase-admin');
+var serviceAccount = require('./three-words-cec8e-firebase-adminsdk-fyd5t-72296c0595.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://three-words-cec8e.firebaseio.com/'
+});
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,4 +18,11 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+var db = admin.database();
+var ref = db.ref("bad-words");
+
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
 });
